ad2e434818b0202b2a9aa7950430860283cd5ed8
NnWinter <515621078@163.com>
2023-02-15 20:46:28 +0800

    分离CFocusTree + 修复Csv读取
    1. CFocusTree 放在单独的类里
    2. 读 Csv 的时候还是要跳过第一行

--- FocusTree/IO/FtCsv.cs
+++ FocusTree/IO/FtCsv.cs
@@ -16,15 +16,22 @@ namespace FocusTree.IO
         /// <returns>string[][] csv纯文本表格</returns>
         public static string[][] ReadCsv(string filePath)
         {
+            // csv 读取设置
+            var settings = new CSVSettings
+            {
+                HeaderRowIncluded = false // 第一行不做标头
+            };
+
+            // 返回读取结果
             string[][] data;
             try
             {
-                CSVReader csvData = CSVReader.FromFile(filePath);
+                CSVReader csvData = CSVReader.FromFile(filePath, settings);
                 data = csvData.ToArray(); //作为二维数组返回
             }
             catch (Exception ex)
             {
-                throw new Exception($"读取CSV文件失败：{ex.Message}");
+                throw new Exception($"[2302152043] 读取CSV文件失败：{ex.Message}");
             }
             return data;
         }
--- FocusTree/UI/TreeMap.cs
+++ FocusTree/UI/TreeMap.cs
@@ -9,6 +9,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Serialization;
+using FocusTree.Tree;
 
 namespace FocusTree
 {
