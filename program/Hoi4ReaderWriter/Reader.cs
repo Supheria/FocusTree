2762f107045778f014f986e32714c9e739a16162
Supheria <Supheria@gmail.com>
2023-05-06 14:10:04 +0800

    删除了废弃代码，修复了读取引号内 UTF-8 乱码的问题

--- program/Hoi4ReaderWriter/Reader.cs
+++ program/Hoi4ReaderWriter/Reader.cs
@@ -8,44 +8,55 @@ using System.Threading.Tasks;
 
 namespace Hoi4ReaderWriter
 {
-    public class Reader
+    public class Hoi4Reader
     {
+        /// <summary>
+        /// 节点类型
+        /// </summary>
         public enum ElementTypes
         {
             Element,
-            StartElement,
+            //StartElement,
             EndElement
         }
+        /// <summary>
+        /// 节点名称
+        /// </summary>
         public string Name { get; private set; } = string.Empty;
+        /// <summary>
+        /// 节点类型
+        /// </summary>
         public ElementTypes Type { get; private set; }
+        /// <summary>
+        /// 节点值（StartElement&EndElement类型节点值为 string.Empty)
+        /// </summary>
         public string Value { get; private set; } = string.Empty;
+        /// <summary>
+        /// 记录节点由内外顺序
+        /// </summary>
         Stack<string> StartElements = new();
+        /// <summary>
+        /// 测试用 - 当前节点缩进级别
+        /// </summary>
         public int TabTimes { get; private set; } = 0;
         /// <summary>
         /// 裁剪流
         /// </summary>
         MemoryStream TrimedStream = new();
-        public Reader(Stream rawStream)
-        {
-            ReadTrimedStream(rawStream);
-#if DEBUG
-            FileStream file = new("C:\\Users\\Non_E\\Documents\\GitHub\\FocusTree\\FocusTree\\program\\FILES\\test reader writer\\class out put.txt", FileMode.Create);
-            file.Write(TrimedStream.ToArray());
-            foreach (var c in TrimedStream.ToArray())
-            {
-                //Console.Write((char)c);
-            }
-#endif
-        }
-        private bool IsBlank(int c)
-        {
-            return c == '\n' || c == ' ' || c == '\t' || c == '\r';
-        }
 
         #region ==== 初始化裁剪流 ====
 
+        public Hoi4Reader(Stream rawStream)
+        {
+            ReadTrimedStream(rawStream);
+#if DEBUG
+            FileStream file = new("trimed stream.txt", FileMode.Create);
+            file.Write(TrimedStream.ToArray());
+#endif
+        }
         private void ReadTrimedStream(Stream stream)
         {
+            bool lastIsBlank = false;
             int currentByte;
             while ((currentByte = stream.ReadByte()) != -1)
             {
@@ -53,34 +64,17 @@ namespace Hoi4ReaderWriter
                 {
                     while ((currentByte = stream.ReadByte()) != '\n') ;
                 }
-                if (currentByte == '=')
+                if (currentByte == '\n' || currentByte == ' ' || currentByte == '\t' || currentByte == '\r')
                 {
-                    TrimedStream.WriteByte((byte)currentByte);
-                    while (IsBlank(currentByte = stream.ReadByte())) ;
-                    if (currentByte == -1)
+                    if (!lastIsBlank)
                     {
-                        throw new EndOfStreamException("No any value.");
+                        lastIsBlank = true;
+                        TrimedStream.WriteByte((byte)' ');
                     }
-                    TrimedStream.WriteByte((byte)currentByte);
-                    while ((currentByte = stream.ReadByte()) != '\n')
-                    {
-                        if (currentByte == '\r' || currentByte == -1) { break; }
-                        if (currentByte == '#')
-                        {
-                            while (stream.ReadByte() != '\n') ;
-                            break;
-                        }
-                        TrimedStream.WriteByte((byte)currentByte);
-                    }
+                    continue;
                 }
-                else if (!IsBlank(currentByte))
-                {
-                    TrimedStream.WriteByte((byte)currentByte);
-                }
-                else
-                {
-                    TrimedStream.WriteByte((byte)' ');
-                }
+                TrimedStream.WriteByte((byte)currentByte);
+                lastIsBlank = false;
             }
             TrimedStream.Position = 0;
         }
@@ -94,80 +88,96 @@ namespace Hoi4ReaderWriter
             if (currentByte == -1) { return false; }
             if (currentByte == '}')
             {
-                Name = StartElements.Pop();
                 Type = ElementTypes.EndElement;
+                Name = StartElements.Pop();
                 Value = string.Empty;
                 TabTimes = StartElements.Count;
                 return true;
             }
-            StringBuilder sb = new();
+            Type = ElementTypes.Element;
             TabTimes = StartElements.Count;
+            StringBuilder sb = new();
+            bool hasBlank = false;
             do
             {
-                sb.Append((char)currentByte);
+                if (currentByte != ' ')
+                {
+                    if (currentByte == -1) { throw new InvalidDataException("No Element Name was found."); }
+                    if (hasBlank) { throw new InvalidDataException("Element Name cannot contain blank."); }
+                    sb.Append((char)currentByte);
+                }
+                else { hasBlank = true; }
             } while ((currentByte = TrimedStream.ReadByte()) != '=' && currentByte != '<' && currentByte != '>');
-            if (sb.Length == 0) { throw new InvalidDataException("No Element was found."); }
+            if (sb.Length == 0) { throw new InvalidDataException("No Element Name was found."); }
             sb.Append((char)currentByte); // = or < or >
             Name = sb.ToString()[..^1].Trim();
             while ((currentByte = TrimedStream.ReadByte()) == ' ') ;
             if (currentByte == '{')
             {
-                if (SearchForEqualSign())
+                if (SearchForAssignmentWithinBrace())
                 {
+                    //Type = ElementTypes.StartElement;
                     StartElements.Push(Name);
                     TrimedStream.ReadByte();
-                    Type = ElementTypes.StartElement;
                     Value = string.Empty;
                     return true;
                 }
                 // enum condition
                 Type = ElementTypes.Element;
-                sb = new(sb.ToString().Substring(sb.Length - 1, 1));
+                sb = new(sb.ToString().Substring(sb.Length - 1, 1)); // new since = or < or >
+                hasBlank = false;
                 while ((currentByte = TrimedStream.ReadByte()) != '}')
                 {
                     if (currentByte == ' ') 
                     {
-                        while ((currentByte = TrimedStream.ReadByte()) == ' ') ;
-                        if (currentByte == '}') { break; }
-                        sb.Append('|');
+                        hasBlank = true;
                         continue; 
+                    }
+                    if (hasBlank)
+                    {
+                        sb.Append(',');
+                        hasBlank = false;
                     }
                     sb.Append((char)currentByte);
                 }
-                //TrimedStream.Position--;
                 Value = sb.ToString();
                 return true;
             }
-            Type = ElementTypes.Element;
-            //sb.Clear();
-            sb = new(sb.ToString().Substring(sb.Length - 1, 1));
-            // "... ..."
+            //Type = ElementTypes.Element;
+            sb = new(sb.ToString().Substring(sb.Length - 1, 1)); // new since = or < or >
+            // "... ..." or "UTF-8" condition
             if (currentByte == '\"')
             {
+                var buffer = new byte[1024];
+                int realLength = 0;
                 while ((currentByte = TrimedStream.ReadByte()) != '\"')
                 {
-                    sb.Append((char)currentByte);
+                    buffer[realLength++] = (byte)currentByte;
                 }
+                sb.Append(Encoding.UTF8.GetString(buffer, 0, realLength));
+                Value = sb.ToString();
+                return true;
             }
-            else
+            if (currentByte == ' ')
             {
-                if (currentByte == ' ')
-                {
-                    while ((currentByte = TrimedStream.ReadByte()) == ' ') ;
-                }
-                do
-                {
-                    sb.Append((char)currentByte);
-                } while ((currentByte = TrimedStream.ReadByte()) != ' ' && currentByte != '}');
-                if (currentByte == '}')
-                {
-                    TrimedStream.Position--;
-                }
+                while ((currentByte = TrimedStream.ReadByte()) == ' ') ;
+            }
+            do
+            {
+                sb.Append((char)currentByte);
+            } while ((currentByte = TrimedStream.ReadByte()) != ' ' && currentByte != '}');
+            if (currentByte == '}')
+            {
+                TrimedStream.Position--;
             }
             Value = sb.ToString();
             return true;
         }
-        private bool SearchForEqualSign()
+        /// <summary>
+        /// 在花括号内预查找是否有赋值语句（ = or < or > ）
+        /// </summary>
+        /// <returns></returns>
+        private bool SearchForAssignmentWithinBrace()
         {
             var streamPositionCache = TrimedStream.Position;
             int currentByte;
