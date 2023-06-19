054e12caa63acb1893307a8fd51e4c10f526921a
Supheria <Supheria@gmail.com>
2023-06-20 03:33:54 +0800

    分离了 Parser 项目

--- program/FocusTree/Program.cs
+++ program/FocusTree/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 #define MAIN
+using FocusTree;
 using FocusTree.Data.Focus;
 using FocusTree.IO;
 using FocusTree.UI;
